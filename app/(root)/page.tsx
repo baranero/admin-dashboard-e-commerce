be3494f0e92d84953f3830aaa25a6e5145f66f10
baranero <jakub.baran2@gmail.com>
2023-12-11 22:50:39 +0100

    added modal component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,20 @@
 "use client";
 
 import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 const SetupPage = () => {
-  return (
-    <div className="p-4">
-      <Modal title="Test" description="Test Description" isOpen onClose={() => {}}>
-        Children
-      </Modal>
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+useEffect(() => {
+  if (!isOpen) {
+    onOpen()
+  }
+}, [isOpen, onOpen])
+  
+  return <div className="p-4">Root Page</div>;
 };
 
 export default SetupPage;
-
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,25 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/providers/modal-provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Admin Dashboard',
-  description: 'Admin Dashboard',
-}
+  title: "Admin Dashboard",
+  description: "Admin Dashboard",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
