39df60a87bd96772254db4a43aa0ca18b7ed0550
baranero <jakub.baran2@gmail.com>
2023-12-13 22:21:47 +0100

    add react toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-provider";
+import { ToasterProvider } from "@/providers/toast-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ToasterProvider />
           <ModalProvider />
           {children}
         </body>
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -17,6 +17,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 const formSchema = z.object({
   name: z.string().min(1),
@@ -40,12 +41,12 @@ export const StoreModal = () => {
 
       const response = await axios.post('/api/stores', values)
 
-      console.log(response.data);
+      toast.success("Store created.")
       
 
     } catch (error) {
       console.log(error);
-      
+      toast.error("Something went wrong.")
     } finally {
       setLoading(false)
     }
