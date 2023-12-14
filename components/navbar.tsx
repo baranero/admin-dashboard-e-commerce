158660552747078deca98ddafd30d4b1ac5d3010
baranero <jakub.baran2@gmail.com>
2023-12-14 20:00:29 +0100

    create navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,18 @@
+import { UserButton } from "@clerk/nextjs";
+import { MainNav } from "./main-nav";
+
+const Navbar = () => {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <div>This will be a store switcher</div>
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
