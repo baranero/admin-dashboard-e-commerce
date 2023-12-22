cfceaca2e26a30a34edd6d5100ed1b51426533fd
baranero <jakub.baran2@gmail.com>
2023-12-22 12:10:32 +0100

    added settings form

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -46,7 +46,7 @@ export const SettingsForm: FC<SettingsFormProps> = ({ initialData }) => {
     <>
       <div className="flex items-center justify-between">
         <Heading title="Settings" description="Manage store preferences" />
-        <Button variant="destructive" size="icon" onClick={() => {}}>
+        <Button variant="destructive" size="icon" onClick={() => setOpen(true)}>
           <Trash className="h-4 w-4" />
         </Button>
       </div>
