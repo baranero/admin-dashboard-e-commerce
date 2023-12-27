80800a45c860e45fc3004371874ef207ae3f86e2
baranero <jakub.baran2@gmail.com>
2023-12-27 14:29:45 +0100

    created products page

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/cell-action.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { FC, useState } from "react";
+import { ProductColumn } from "./columns";
+import { AlertModal } from "@/components/modals/alert-modal";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+import toast from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
+import axios from "axios";
+
+interface CellActionProps {
+  data: ProductColumn;
+}
+
+export const CellAction: FC<CellActionProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onCopy = (id: string) => {
+    navigator.clipboard.writeText(id);
+    toast.success('Product ID copied to clipboard.');
+  }
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/products/${data.id}`);
+      router.refresh();
+
+      toast.success("Product deleted.");
+      
+    } catch (error) {
+      toast.error('Something went wrong');
+    } finally {
+      setOpen(false);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open Menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem onClick={() => onCopy(data.id)}>
+            <Copy className="mr-2 h-4 w-4" />
+            Copy ID
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => router.push(`/${params.storeId}/products/${data.id}`)}
+          >
+            <Edit className="mr-2 h-4 w-4" />
+            Update
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="text-red-600"
+            onClick={() => setOpen(true)}
+          >
+            <Trash className="mr-2 h-4 w-4" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
---
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { FC } from "react";
+import { ProductColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+interface ProductsClientProps {
+  data: ProductColumn[];
+};
+
+export const ProductsClient: FC<ProductsClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <> 
+      <div className="flex items-center justify-between">
+        <Heading title={`Products (${data.length})`} description="Manage products for your store" />
+        <Button onClick={() => router.push(`/${params.storeId}/products/new`)}>
+          <Plus className="mr-2 h-4 w-4" /> Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description="API Calls for Products" />
+      <Separator />
+      <ApiList entityName="products" entityIdName="productId" />
+    </>
+  );
+};
