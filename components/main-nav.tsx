3a5ffe66122922c82550e44a4e65772bcb511abb
baranero <jakub.baran2@gmail.com>
2023-12-28 12:36:10 +0100

    added orders page

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -41,6 +41,11 @@ export function MainNav({ className, ...props }: HTMLAttributes<HTMLElement>) {
       active: pathname === `/${params.storeId}/products`,
     },
     {
+      href: `/${params.storeId}/orders`,
+      label: 'Orders',
+      active: pathname === `/${params.storeId}/orders`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathname === `/${params.storeId}/settings`,
